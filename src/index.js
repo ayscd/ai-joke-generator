7445ac6825e3a521711798d5381aed35bef5d856
ayscd <ayeskacsta@gmail.com>
2025-03-31 09:14:44 -0300

    Update 2 - API adjustments
    - adjusting result managemet
    - setting up the result display
    - improving the prompt

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-//let joke = document.querySelector("#joke");
+function displayJoke(result) {
+    let joke = document.querySelector("#joke");
+    answer = result.data.answer;
+    console.log(answer);
+
+    joke.innerHTML = answer;
+}
 
 function tellMeJoke(event) {
     event.preventDefault();
 
     let apiKey = "do8ae0f5a33a8b61b41f1t1ed44678b4";
-    let prompt = `Tell me a joke, but make it smart, objective and short. Write the joke in the format of 2 lines of HTML, the first line is the 'question' of the joke, the second line is the 'answer'. Be polite.`;
-    let context = "You're a brazillian comediant, all your jokes must be written in brazillian portuguese. You are inspired by figures like 'Tata Werneck', 'Rafael Portugal', 'Mizael Alves'. Your humor is smart, and you're polite";
+    let prompt = 'Tell me a joke, but make it objective. Write the joke in the format of 2 lines. The first line id and class must be question, and is contained by a <div></div>. The second line id and class must be answer, and is contained by a <div></div>. Be polite.';
+    let context = 'You are a brazillian comediant, all your jokes must be written in brazillian portuguese. You are inspired by figures like Tata Werneck and Rafael Portugal.';
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     console.log(apiUrl);
